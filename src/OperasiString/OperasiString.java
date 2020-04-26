e5b0eb8bbbad4cd40f258db4b113665d4dca90bd
julianitadaraandriani <darmingg11@gmail.com>
2020-04-26 21:16:11 +0800

    Buat Class OperasiString

---
+++ src/OperasiString/OperasiString.java
@@ -0,0 +1,9 @@
+package OperasiString;
+
+public class OperasiString {
+    public static void main(String[] args) {
+        String kota = "Yogyakarta" ;
+
+        System.out.println(kota);
+    }
+}
